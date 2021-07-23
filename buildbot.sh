467857d8efe42b063279ced97939c5e52f42a9b4
boedhack99 (dblenk) <boedhack@gmail.com>
2021-07-23 13:17:43 +0700

    [skip ci]

--- buildbot.sh
+++ buildbot.sh
@@ -1,11 +1,11 @@
 # Rom building script for CircleCI
 # coded by bruh™ aka Live0verfl0w
 
-MANIFEST_LINK=https://github.com/PixelOS-Pixelish
-BRANCH=eleven
-ROM_NAME=Pixel
-GITHUB_USER=I-n-o-k
-GITHUB_EMAIL=inok.dr189@gmail.com
+MANIFEST_LINK=https://github.com/AospExtended/manifest
+BRANCH=11.x
+ROM_NAME=AospExtended
+GITHUB_USER=boedhack99
+GITHUB_EMAIL=boedhack@gmail.com
 WORK_DIR=$(pwd)/${ROM_NAME}
 JOBS=nproc
 
@@ -17,13 +17,13 @@ git config --global user.email ${GITHUB_EMAIL}
 mkdir ${WORK_DIR} && cd ${WORK_DIR}
 
 device
-git clone --depth=1 https://github.com/MiBengal-Development/Device_xiaomi_J --single-branch -b PixelOs device/xiaomi/juice
-git clone --depth=1 https://github.com/I-n-o-k/prebuilt_vendor_juice --single-branch -b 11 vendor/xiaomi/juice
-git clone --depth=1 https://github.com/MiBengal-Development/android_kernel_xiaomi_bengal  --single-branch -b 11.0 kernel/xiaomi/juice
-git clone --depth=1 https://github.com/GengKapak/GengKapak-clang --single-branch -b 13 prebuilts/clang/host/linux-x86/clang-gengkapak
+git clone --depth=1 https://github.com/boedhack99/local_manifest -b R --single-branch -b .repo/local_manifests
+#git clone --depth=1 https://github.com/I-n-o-k/prebuilt_vendor_juice --single-branch -b 11 vendor/xiaomi/juice
+#git clone --depth=1 https://github.com/MiBengal-Development/android_kernel_xiaomi_bengal  --single-branch -b 11.0 kernel/xiaomi/juice
+#git clone --depth=1 https://github.com/GengKapak/GengKapak-clang --single-branch -b 13 prebuilts/clang/host/linux-x86/clang-gengkapak
 
 # Start building!
 . build/envsetup.sh
-lunch aosp-juice-userdebug
-mka bacon -j${JOBS}
+lunch aosp_mojito-userdebug
+m aex  -j${JOBS}
 
